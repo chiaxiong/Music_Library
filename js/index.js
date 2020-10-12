2d7fe67fc9cd5b5fe1bbcaa467e248c5a7847393
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-10-12 14:51:23 -0500

    Added table tags and test js code.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+$(document).ready(function() {
+    // $("td").append(data[0].title)
+});
+
+function getMusiclibrary() {
+    $.ajax({
+        url: 'http://www.devcodecampmusiclibrary.com/api/music',
+        dataType: 'json',
+        type: 'get',
+        success: function(data, textStatus, jqXHR) {
+            $("td").append(" hello");
+        },
+        error: function(errorThrown) {
+            console.log(errorThrown);
+        }
+    })
+}
+
+getMusiclibrary();

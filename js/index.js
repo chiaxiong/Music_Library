c8b882852ef22cd403afb5fe05bdaa0fda177f99
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-10-12 16:02:22 -0500

    added jquery to append from data to display in browswer.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,58 @@
 $(document).ready(function() {
-    // $("td").append(data[0].title)
-});
-
-function getMusiclibrary() {
+    let $data = $('.title');
     $.ajax({
         url: 'http://www.devcodecampmusiclibrary.com/api/music',
         dataType: 'json',
         type: 'get',
         success: function(data, textStatus, jqXHR) {
-            $("td").append(" hello");
+            $.each(data, function(i, list) {
+                $data.append('<td>Title: ' + list.title + ' </td>')
+            });
+
         },
         error: function(errorThrown) {
             console.log(errorThrown);
         }
-    })
-}
+    });
+});
 
-getMusiclibrary();
+// $(document).ready(function() {
+//     let $data = $('.album');
+//     $.ajax({
+//         url: 'http://www.devcodecampmusiclibrary.com/api/music',
+//         dataType: 'json',
+//         type: 'get',
+//         success: function(data, textStatus, jqXHR) {
+//             $.each(data, function(i, list) {
+//                 $data.append('<td>Album: ' + list.album + ' </td>')
+//             });
+
+//         },
+//         error: function(errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+// });
+
+// $(document).ready(function() {
+
+//     let $data = $('#music-library');
+//     $.ajax({
+//         url: 'http://www.devcodecampmusiclibrary.com/api/music',
+//         dataType: 'json',
+//         type: 'get',
+//         success: function(data, textStatus, jqXHR) {
+//             $.each(data, function(i, list) {
+//                 $data.append('<tr><td>Title: ' + list.title + ' </tr></td>');
+//                 $data.append('<tr><td>Album: ' + list.album + ' </tr></td>');
+//                 $data.append('<tr><td>Artist: ' + list.artist + ' </tr></td>');
+//                 $data.append('<tr><td>Genre: ' + list.genre + ' </tr></td>');
+//                 $data.append('<tr><td>Release Date: ' + list.release + ' </tr></td>');
+//             });
+
+//         },
+//         error: function(errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+// });
